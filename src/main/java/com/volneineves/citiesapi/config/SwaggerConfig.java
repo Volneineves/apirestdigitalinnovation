64f09b17968094687779484033641a2b1db96edb
VOLNEI NEVES FILHO <volneinevesfilho@hotmail.com>
2021-10-19 22:07:52 -0300

    UPDATE Swagger

--- src/main/java/com/volneineves/citiesapi/config/SwaggerConfig.java
+++ src/main/java/com/volneineves/citiesapi/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.volneineves.citiesapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,12 +14,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
-    public Docket api() {
+    public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
+                //.apis(RequestHandlerSelectors.basePackage("com.volneineves.citiesapi.controllers"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title("CIDADES DOS BRASIL")
+                .description("REST API para consulta de cidades e estados do Brasil")
+                .version("1.0.0")
+                .contact(new Contact(
+                        "Volnei Neves",
+                        "https://github.com/volneineves/apirestdigitalinnovation",
+                        null))
                 .build();
     }
 }
